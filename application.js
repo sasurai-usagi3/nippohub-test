5e38585ca13308b9b66d1277e98dd7e045a47cbd
宇山 拓夢 <sasurai.usagi3@gmail.com>
2018-12-30 08:29:43 +0900

    feature: 特定のユーザのもののみ取得

--- application.js
+++ application.js
@@ -8,7 +8,7 @@ auth.onAuthStateChanged(currentUser => {
   if(currentUser != null) {
     pageAuth.setAttribute('hidden', 'hidden');
     pageMain.removeAttribute('hidden');
-    init();
+    init(currentUser.uid);
   } else {
     ui.start('#js-form-auth-area', {
       signInSuccessUrl: '/',
@@ -21,13 +21,13 @@ auth.onAuthStateChanged(currentUser => {
   }
 });
 
-const init = () => {
+const init = (userId) => {
   const database = firebase.database();
   const form = document.getElementById('js-form-memo');
   const memoTextField = document.getElementById('js-content-text-field');
   const listMemo = document.getElementById('js-list-memo');
 
-  database.ref('memos').on('value', r => {
+  database.ref('memos').orderByChild('userId').equalTo(userId).on('value', r => {
     const data = r.val();
 
     for(let i = listMemo.children.length - 1; i >= 0; --i) {
@@ -46,7 +46,8 @@ const init = () => {
   form.addEventListener('submit', e => {
     e.preventDefault();
     database.ref('memos').push({
-      contents: memoTextField.value
+      contents: memoTextField.value,
+      userId: userId
     });
     memoTextField.value = '';
   });
